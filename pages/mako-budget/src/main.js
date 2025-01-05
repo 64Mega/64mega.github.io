7cbf100006840c17278845b09bde1796b1c12e9e
Astrid Lawrence <astryl.lawrence@gmail.com>
2025-01-05 13:58:01 +0200

    Link manifest

--- pages/mako-budget/src/main.js
+++ pages/mako-budget/src/main.js
@@ -1,4 +1,10 @@
 import {App} from "./app.js";
 
+if (!navigator.serviceWorker?.controller) {
+    navigator.serviceWorker.register("https://64mega.github.io/pages/mako-budget/sw.js").then(function (reg) {
+        console.log("Service worker has been registered for scope: " + reg.scope);
+    });
+}
+
 const app = new App();
 app.attach(document.body);
